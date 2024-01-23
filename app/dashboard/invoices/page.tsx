2c56350b09a4008c72e6ec28ad5de0749d3cb2ce
Vlad <voiteh44@gmail.com>
2024-01-23 20:11:49 +0300

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Page = () => {
+   return (
+      <div>
+         invoices
+      </div>
+   )
+}
+export default Page;
+
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Page = () => {
+   return (
+      <div>
+         dashboard afdfdsfs
+      </div>
+   )
+}
+export default Page;
